542a7a020a684b12b70a38efb45a1380d83739c9
Anand <anand2522004@gmail.com>
2025-07-03 18:10:26 +0530

    Updated vite.config.js with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio/', // 👈 VERY important: this must match your repo name!
   plugins: [react()],
-  base: '/portfolio/', // 👈 matches your GitHub repo name
 })
